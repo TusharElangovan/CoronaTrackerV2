86b5cf7169851ab0238e42da902a84ecc67d0f5e
Tushar <tushare0509@gmail.com>
2020-07-14 13:37:18 +0530

    Initial Commit

---
+++ CoronaTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaTracker
+//
+//  Created by Tushar Elangovan on 7/14/20.
+//  Copyright © 2020 Tushar Elangovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
